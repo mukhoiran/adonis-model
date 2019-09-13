0f5d8c93c86c479306e20f3b45b01ad8872c83f0
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-09-13 09:30:43 +0700

    passing data from Model

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,18 +1,23 @@
 'use strict'
+//load model Post
+const Post = use('App/Models/Post')
 
 class PostController {
   // async index(){
   //   return 'Posts'
   // }
   async index({ view }){
-    const posts = [
-      {title: 'Post One', body: 'This is post one'},
-      {title: 'Post Two', body: 'This is post two'},
-      {title: 'Post Three', body: 'This is post three'},
-    ]
+    // const posts = [
+    //   {title: 'Post One', body: 'This is post one'},
+    //   {title: 'Post Two', body: 'This is post two'},
+    //   {title: 'Post Three', body: 'This is post three'},
+    // ]
+
+    const posts = await Post.all();
+
     return view.render('posts.index',{
       title: 'Latest Post',
-      posts: posts
+      posts: posts.toJSON()
     })
   }
 }
